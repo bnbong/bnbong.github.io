db7bffcf823e64036aba0b8bbe30f19eea38b0ea
bnbong <bbbong9@gmail.com>
2020-05-22 22:42:04 +0900

    assignment 1

---
+++ 7th/app/src/main/java/com/example/weatherforecast/ForecastFragment.java
@@ -0,0 +1,46 @@
+package com.example.weatherforecast;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.weatherforecast.ui.main.MainViewModel;
+
+import java.util.ArrayList;
+
+public class ForecastFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+    private ListView myListView;
+    private ArrayList<String> sample_data;
+    private ArrayAdapter<String> listViewAddapter;
+
+    public static ForecastFragment newInstance() {
+        return new ForecastFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ 7th/app/src/main/java/com/example/weatherforecast/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.weatherforecast.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
