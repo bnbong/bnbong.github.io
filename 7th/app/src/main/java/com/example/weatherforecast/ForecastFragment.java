179e60e6190a9e6252bc1eb6441e6952bfd3c962
bnbong <bbbong9@gmail.com>
2020-05-26 23:13:55 +0900

    assignment 2

--- 7th/app/src/main/java/com/example/weatherforecast/ForecastFragment.java
+++ 7th/app/src/main/java/com/example/weatherforecast/ForecastFragment.java
@@ -1,7 +1,11 @@
 package com.example.weatherforecast;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,19 +15,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.weatherforecast.ui.main.MainViewModel;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ForecastFragment extends Fragment {
 
     private MainViewModel mViewModel;
-    private ListView myListView;
     private ArrayList<String> sample_data;
     private ArrayAdapter<String> listViewAddapter;
+    private ArrayAdapter<String> mForecastAdapter;
+
 
     public static ForecastFragment newInstance() {
         return new ForecastFragment();
@@ -33,7 +42,38 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        String[] data = {
+                "Mon 6/23 - Sunny 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        mForecastAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_view_item,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+
+        View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                intent.putExtra("data", forecast);
+            }
+        });
+
+        return rootView;
     }
 
     @Override
@@ -42,5 +82,6 @@ public class ForecastFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         // TODO: Use the ViewModel
     }
+
 
 }
